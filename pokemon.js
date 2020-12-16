968c4bd641d4ab807891f99aee384b9b5fde8b51
Daniel Buergler <daniel.jbuergler@gmail.com>
2020-12-15 19:28:45 -0500

    first commit

---
+++ pokemon.js
@@ -0,0 +1,63 @@
+document.getElementsByTagName('form')[0].addEventListener('submit', fetchPokemon)
+
+let ul = document.querySelector('ul');
+let pokemonList = document.querySelector('#pokemonList');
+
+
+function fetchPokemon(e) {
+
+    e.preventDefault();
+   
+    let input = document.querySelector('#search');
+    let singleMan = input.value.toLowerCase();
+    let baseURL = `https://pokeapi.co/api/v2/pokemon/${singleMan}/`
+    fetch(baseURL)
+    .then(res => res.json())
+    .then(json =>{
+        console.log(json);
+        displayPokemon(json)
+    })
+    .catch(err=>console.log(err));
+
+   
+}
+
+
+function displayPokemon (pokemon) {
+    $("#picture").attr("src", pokemon.sprites.front_default);
+    pokemon.forEach(p => {
+        let pokemonAbilities = document.createElement('li')
+        pokemonAbilities.innerText = p.abiliies.ability.name;
+        pokemonList.appendChild(pokemonAbilities);
+
+    })
+}   
+
+
+
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
